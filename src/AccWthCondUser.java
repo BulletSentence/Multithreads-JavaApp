a1eb1182d04aa024480d91722efcf81651851e1c
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-08 15:45:21 -0300

    Account Class created

--- src/AccWthCondUser.java
+++ src/AccWthCondUser.java
@@ -1,5 +1,22 @@
 public class AccWthCondUser {
     public static void main(String[] args) {
-        
+
     }
+
+
+    private static class Account {
+        private int balance = 0;
+        public int getBalance(){
+            return balance;
+        }
+
+        public void withdraw(int amount){
+
+        }
+
+        public void deposit(int amount){
+
+        }
+    }
+
 }
