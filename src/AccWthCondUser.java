4802550a07360ee01cbbf701aacb0989815be974
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-08 15:47:56 -0300

    WithdrawTask class created

--- src/AccWthCondUser.java
+++ src/AccWthCondUser.java
@@ -1,8 +1,18 @@
 public class AccWthCondUser {
+    private static Account account = new Account();
     public static void main(String[] args) {
 
     }
 
+
+
+    public static class WithdrawTask implements Runnable {
+        public void run() {
+            while (true) {
+                account.withdraw((int)(Math.random() * 10) + 1);
+            }
+        }
+    }
 
     private static class Account {
         private int balance = 0;
