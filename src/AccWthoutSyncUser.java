47596edcdf3514c4d453529b6119740b04e3a393
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-08 16:01:46 -0300

    Account Without SyncUser

---
+++ src/AccWthoutSyncUser.java
@@ -0,0 +1,32 @@
+public class AccWthoutSyncUser {
+    private static Account userAccount = new Account();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++){
+
+        }
+        while (!executor.isShutdown()){
+
+        }
+        System.out.println("What is balance? " + userAccount.getBalance());
+    }
+
+    private static class AddPenny implements Runnable {
+        public void run(){
+            userAccount.deposit(1);
+        }
+    }
+
+    private static class Account {
+        private int balance = 0;
+
+        public void deposit(int amount) {
+            int newBalance = balance + amount;
+            balance = newBalance;
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+    }
+}
